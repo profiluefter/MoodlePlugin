9253718aa4564949cad6b034717fd274bf6a24af
Fabian <fabian.paul.gurtner@gmail.com>
2020-02-04 19:39:05 +0100

    Find moodle course endpoint

--- src/main/java/me/profiluefter/moodlePlugin/moodle/Moodle.java
+++ src/main/java/me/profiluefter/moodlePlugin/moodle/Moodle.java
@@ -3,56 +3,61 @@ package me.profiluefter.moodlePlugin.moodle;
 import me.profiluefter.moodlePlugin.moodle.data.Course;
 import me.profiluefter.moodlePlugin.moodle.data.MoodleToken;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Moodle {
-	private List<Course> courses;
+	private final Map<Integer, Course> courses;
 
-	private MoodleHost host;
-	private MoodleToken token;
+	private final MoodleHost host;
+	private final MoodleToken token;
 
 	Moodle(MoodleHost host, MoodleToken token) {
-		this.courses = new ArrayList<>();
+		this.courses = new HashMap<>();
 
 		this.host = host;
 		this.token = token;
 	}
 
-	public List<Course> getCourses() {
-		return getCourses(false);
+	public Course getCourseByID(int id) {
+		return getCourseByID(id,false);
 	}
 
-	public List<Course> getCourses(boolean forceRefresh) {
-		throw new IllegalStateException("TODO");
+	public Course getCourseByID(int id, boolean forceRefresh) {
+		if(forceRefresh || !courses.containsKey(id)) {
+			System.out.println(callMoodleFunction("core_course_get_contents", "courseid="+id));
+		}
+		return courses.get(id);
 	}
 
-	public JSONObject callMoodleFunction(String functionName, JSONObject parameters) throws IOException {
-		URI endpoint = host.getMoodlePath().resolve(
-				String.format(
-						"lib/ajax/service.php?info=%s",
-						functionName
-				)
-		);
-
-		JSONArray payload = new JSONArray().put(0, new JSONObject().put("args", parameters).put("index", 0).put("methodname", functionName));
-
-		HttpURLConnection connection = ((HttpURLConnection) endpoint.toURL().openConnection());
-		connection.setRequestMethod("POST");
-		connection.connect();
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-		writer.write(payload.toString());
-		writer.close();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String response = reader.lines().collect(Collectors.joining("\n"));
-		System.out.println(response);
-		return null;
+	public JSONArray callMoodleFunction(String functionName, String payload) {
+		try {
+			URI endpoint = host.getMoodlePath().resolve(
+					String.format(
+							"webservice/rest/server.php?wstoken=%s&wsfunction=%s&moodlewsrestformat=json&%s",
+							URLEncoder.encode(this.token.getToken(), StandardCharsets.UTF_8),
+							URLEncoder.encode(functionName, StandardCharsets.UTF_8),
+							payload
+					)
+			);
+			HttpURLConnection connection = ((HttpURLConnection) endpoint.toURL().openConnection());
+			connection.connect();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String response = reader.lines().collect(Collectors.joining("\n"));
+			reader.close();
+			System.out.println(response);
+			return new JSONArray(response);
+		} catch(IOException e) {
+			throw new RuntimeException("Error while calling moodle function",e);
+		}
 	}
 }
--- src/test/java/me/profiluefter/moodlePlugin/moodle/MoodleTest.java
+++ src/test/java/me/profiluefter/moodlePlugin/moodle/MoodleTest.java
@@ -1,12 +1,8 @@
 package me.profiluefter.moodlePlugin.moodle;
 
 import me.profiluefter.moodlePlugin.moodle.data.MoodleToken;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.io.IOException;
 
 import static me.profiluefter.moodlePlugin.moodle.SecretTestProperties.secrets;
 
@@ -18,5 +14,10 @@ public class MoodleTest {
 		MoodleHost host = new MoodleHost(secrets.getProperty("moodle.host"));
 		MoodleToken token = host.authenticate(secrets.getProperty("moodle.username"), secrets.getProperty("moodle.password"));
 		MoodleTest.moodleInstance = host.connect(token);
+	}
+
+	@Test
+	public void getCourses() {
+		moodleInstance.getCourseByID(Integer.parseInt(secrets.getProperty("moodle.course.id")), true);
 	}
 }
