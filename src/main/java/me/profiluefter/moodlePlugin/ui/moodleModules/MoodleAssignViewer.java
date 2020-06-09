e33f699a500a4ccf7b2c6fd579c078b0443d17d9
Fabian <fabian.paul.gurtner@gmail.com>
2020-06-09 18:22:10 +0200

    Try loading CEF

--- src/main/java/me/profiluefter/moodlePlugin/ui/moodleModules/MoodleAssignViewer.java
+++ src/main/java/me/profiluefter/moodlePlugin/ui/moodleModules/MoodleAssignViewer.java
@@ -1,29 +1,39 @@
 package me.profiluefter.moodlePlugin.ui.moodleModules;
 
-import com.intellij.openapi.roots.ui.componentsList.components.ScrollablePanel;
 import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.jcef.JBCefApp;
+import com.intellij.ui.jcef.JBCefBrowser;
+import com.intellij.ui.jcef.JBCefClient;
 import me.profiluefter.moodlePlugin.moodle.modules.MoodleAssignModule;
+import org.cef.CefApp;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MoodleAssignViewer extends MoodleModuleViewer {
 	private JPanel guiBuilderPanel;
-	private JLabel name;
+	private JLabel taskDescription;
 
+	@SuppressWarnings("UnstableApiUsage")
 	public MoodleAssignViewer(MoodleAssignModule module) {
 		super(module);
+
+		if(!JBCefApp.isSupported())
+			throw new RuntimeException("No CEF!");
+
+		CefApp.startup(new String[0]);
+		JBCefClient client = JBCefApp.getInstance().createClient();
+		JBCefBrowser browser = new JBCefBrowser(client, null);
+		browser.loadHTML(module.getDescription());
+
 		panel.setLayout(new BorderLayout());
-		ScrollablePanel scrollablePanel = new ScrollablePanel(new BorderLayout());
-		scrollablePanel.add(guiBuilderPanel);
-		panel.add(new JScrollPane(scrollablePanel));
+		panel.add(browser.getComponent());
 
-		name.setText("<html>"+module.getDescription()+"</html>");
-
+		browser.openDevtools();
 	}
 
 	private void createUIComponents() {
-		name = new JBLabel();
-		((JBLabel) name).setAllowAutoWrapping(true);
+		taskDescription = new JBLabel();
+		((JBLabel) taskDescription).setAllowAutoWrapping(true);
 	}
 }
